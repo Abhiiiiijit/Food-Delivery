d5df934215f10afc25ea41b7468ff0aca20d6dcf
Abhiiiiijit <abhijeetbosedas560@gmail.com>
2023-05-05 03:56:21 +0530

    Start

---
+++ src/screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+
+export default function home() {
+  return (
+    <div>
+      <div> <NavBar/> </div>
+      <div> Body </div>
+      <div> <Footer/> </div>
+    </div>
+  )
+}
