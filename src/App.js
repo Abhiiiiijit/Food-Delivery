6c69ee6692001bb1b45848887cea9d45a406de2a
Abhiiiiijit <abhijeetbosedas560@gmail.com>
2023-05-06 14:23:02 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import Login from './screens/Login';
 import Home from './screens/home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <div>
-      <Home/>
-    </div>
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
