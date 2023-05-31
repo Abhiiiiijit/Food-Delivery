bf57f513be97681ed352fd5c150a0d2fb7463750
Abhiiiiijit <abhijeetbosedas560@gmail.com>
2023-05-31 11:16:47 +0530

    Backend_Start

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb+srv://Abhijit:Abhijit2001@cluster0.oqq2kn1.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = async () => {
+  await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
+    if (err) {
+      console.log("-----", err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  });
+};
+module.exports = mongoDB;
